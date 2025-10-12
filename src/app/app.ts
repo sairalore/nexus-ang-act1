c7b764e96534345185c37c740393972c172883f6
sairalore <sairalore@hotmail.com>
2025-10-12 16:03:15 -0500

    Limpieza y quitando la actualización automática en package-lock.json de bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('nexus-ang-act1');
+  protected readonly title = signal('Actividad uno');
 }
